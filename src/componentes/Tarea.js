2d9657101edc7538395dfb8f2408841c1689a1f1
Angie Barreto <angiepbg13@gmail.com>
2025-10-18 14:38:42 -0500

    Estructura inicial de la lista de tareas

---
+++ src/componentes/Tarea.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/Tarea.css";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
+const Tarea = ({ texto, completada }) => {
+  return (
+    <div
+      className={
+        completada ? "tarea-contenedor completada" : "tarea-contenedor"
+      }
+    >
+      <div className="tarea-texto">{texto}</div>
+      <div className="tarea-contendedor-iconos">
+        <AiOutlineCloseCircle className="tarea-icono" />
+      </div>
+    </div>
+  );
+};
+
+export default Tarea;
---
+++ src/componentes/TareaFormulario.js
@@ -0,0 +1,17 @@
+import "../styles/TareaFormulario.css";
+
+const TareaFormulario = () => {
+  return (
+    <form className="tarea-formulario">
+      <input
+        className="tarea-input"
+        type="text"
+        placeholder="Escribe una Tarea"
+        name="texto"
+      />
+      <button className="tarea-boton">Agregar Tarea</button>
+    </form>
+  );
+};
+
+export default TareaFormulario;
